3b8002417f1f17ca9ab8e6783367a11d125d87b7
remynixu <remynixu@gmail.com>
2026-08-08 21:16:11 +0800

    cleared up my plans :>

---
+++ src/system/map.h
@@ -0,0 +1,10 @@
+#ifndef MAP_H
+#define MAP_H
+
+/*
+ * Current data structure plan:
+ * - map -> chunks -> tiles
+ * - world -> {abandoned town, mysterious altar -> {goop, altar, floor}}
+ */
+
+#endif /* MAP_H */
