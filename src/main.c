9c42a44b019ced3f88cff12170ce3fef3ca3341e
remynixu <remynixu@gmail.com>
2026-08-10 10:37:02 +0800

    Added a player :D

--- src/main.c
+++ src/main.c
@@ -1,34 +1,43 @@
 #include "system/engine.h"
 #include "system/chunk.h"
-
-struct engine_ctx engine_ctx = {0};
-struct chunk chunk = {0};
-RenderTexture2D game_target = {0};
-
-void init(void){
-    engine_ctx.screen.height = 600;
-    engine_ctx.screen.width = 800;
-    engine_ctx.target_fps = 60;
-    start_engine(&engine_ctx, "Arundel's Adventure");
-    game_target = LoadRenderTexture(engine_ctx.screen.width, engine_ctx.screen.height);
-}
+#include "system/entity.h"
 
 int main(void){
+    struct engine_ctx engine_ctx = {0};
+    struct chunk chunk = {0};
+    struct entity player = {0};
+    RenderTexture2D game_target = {0};
     Rectangle src_rec = {0}, dest_rec = {0};
     Vector2 origin = {0};
     Camera2D cam = {0};
     if(!ChangeDirectory(GetApplicationDirectory()))
         return -1;
-    init();
+    {
+        engine_ctx.screen.height = 600;
+        engine_ctx.screen.width = 800;
+        engine_ctx.target_fps = 60;
+        start_engine(&engine_ctx, "Arundel's Adventure");
+        game_target = LoadRenderTexture(engine_ctx.screen.width, engine_ctx.screen.height);
+    }
     if(load_tile_textures() != 0)
         return -2;
-    if(parse_chunk("assets/chunks/test.chunk", &chunk) != 0)
+    if(load_entity_textures() != 0)
         return -3;
-    src_rec.width = (float)game_target.texture.width;
-    src_rec.height = -(float)game_target.texture.height;
-    dest_rec.width = engine_ctx.screen.width;
-    dest_rec.height = engine_ctx.screen.height;
-    cam.zoom = 1.0f;
+    { /* Player set-up */
+        player.pos.x = engine_ctx.screen.width / 2;
+        player.pos.y = engine_ctx.screen.height / 2;
+        player.type = ENTITY_PLAYER;
+        player.scale = 2.0f;
+    }
+    if(parse_chunk("assets/chunks/test.chunk", &chunk) != 0)
+        return -4;
+    { /* Camera set-up */
+        src_rec.width = (float)game_target.texture.width;
+        src_rec.height = -(float)game_target.texture.height;
+        dest_rec.width = engine_ctx.screen.width;
+        dest_rec.height = engine_ctx.screen.height;
+        cam.zoom = 1.0f;
+    }
     while(!WindowShouldClose()){
         float cam_move;
         update_engine(&engine_ctx);
@@ -49,6 +58,7 @@ int main(void){
                 draw_chunk(&chunk);
             }
             EndMode2D();
+            draw_entity(&player);
         }
         EndTextureMode();
         BeginDrawing();
