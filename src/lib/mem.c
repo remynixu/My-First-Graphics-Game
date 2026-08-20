f1b0985a7f5beb28e3c81939a8ab310f64785888
remynixu <remynixu@gmail.com>
2026-08-20 22:47:14 +0800

    architecture rework begins! >_<

---
+++ src/lib/mem.c
@@ -0,0 +1,12 @@
+#include "mem.h"
+
+static unsigned char mem_pool[MEM_POOLSIZE] = {0};
+
+void *malloc(unsigned long n){
+    void *ptr = NULL;
+    return ptr;
+}
+
+void free(void *ptr){
+    return;
+}
