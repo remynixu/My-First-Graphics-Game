a38b04f22d8c8132c24502c3a443940eab46d52a
remynixu <remynixu@gmail.com>
2026-08-25 19:11:34 +0800

    ._.

--- src/main.c
+++ src/main.c
@@ -1,28 +1,4 @@
-#include "system/engine.h"
-
-#include <string.h>
 
 int main(void){
-    struct engine_ctx ctx;
-    int errcode = 0;
-    {
-        struct engine_hint hint;
-        {
-            hint.window.height = 600;
-            hint.window.width = 800;
-            hint.target_fps = 60;
-        }
-        memcpy(&hint.window.title, "Inchoatus", 10);
-        errcode = engine_init(&ctx, &hint);
-    }
-    if(errcode){
-        engine_perror();
-        return -1;
-    }
-    while(ctx.state == ENGINE_STATE_RUNNING){
-        engine_update(&ctx);
-        engine_render(&ctx);
-    }
-    engine_close(&ctx);
     return 0;
 }
